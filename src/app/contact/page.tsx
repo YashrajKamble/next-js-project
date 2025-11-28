066aed9eac3545f9205666d22bc991b378810591
Yashraj_K <yashrajkamble7727@gmail.com>
2025-11-28 18:23:14 +0530

    added contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,44 @@
+import React from 'react'
+import { getSession } from '../_lib/session'
+import { getContacts } from '../api/contact'
 
-const ContactPages = () => {
+const ContactPage = async () => {
+    const user = await getSession()
+
+    if (!user) {
+        return (
+            <div>
+                Please {" "}
+
+                <a
+                    href="/login"
+                    className='text-blue-600 hover:underline'>
+                    Login
+                </a>{" "}
+                to view contacts
+            </div>
+        )
+    }
+
+    const contacts = await getContacts(user?.id)
+    console.log(contacts)
+    if (!contacts || contacts.length === 0) {
+        return (
+            <div>
+                Please {" "}
+                <a
+                    href="/contact/new"
+                    className='text-blue-600 hover:underline'>
+                    Add a Contact
+                </a>{" "}
+                to your contact lists
+            </div>
+        )
+    }
+
     return (
-        <div>Contact Pages</div>
+        <div>ContactPage</div>
     )
 }
 
-export default ContactPages
+export default ContactPage
