1b42d200c6d98ca57e7673afd98f17a41300b2ef
Yashraj_K <yashrajkamble7727@gmail.com>
2025-12-03 17:35:11 +0530

    done

--- src/app/actions/contact.ts
+++ src/app/actions/contact.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import { createContact, deleteContact } from "../api/contact";
+import { createContact, deleteContact, updateContact } from "../api/contact";
 import { error } from "console";
 import { getSession } from "../_lib/session";
 import { ContactType } from "../_types/contact";
@@ -33,7 +33,24 @@ export const createContactAction = async (
 export const updateContactAction = async (
   prevState: any,
   formData: FormData
-) => {};
+) => {
+  const id = formData.get("id") as string;
+  const user = await getSession();
+
+  const updatedContact: ContactType = {
+    name: formData.get("name") as string,
+    email: formData.get("email") as string,
+    userId: user?.id,
+  };
+  try {
+    await updateContact(id, updatedContact);
+    revalidatePath("/contact");
+    return { success: true };
+  } catch (error) {
+    console.log("Error updating Contact:", error);
+    return { error: "Failed to update Contact" };
+  }
+};
 
 export const deleteContactAction = async (
   prevState: any,
